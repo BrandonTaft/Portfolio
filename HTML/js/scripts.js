214e429a27881bcba3bd52903843e992ed825a0b
Btaft6278 <btaft6278@gmail.com>
2022-08-15 10:41:05 -0400

    adding version 2.0

--- HTML/js/scripts.js
+++ HTML/js/scripts.js
@@ -1,188 +1,59 @@
-/*-----------------------------------------------------------------------------------
+/*!
+* Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
 
-    Theme Name: Zain
-    Theme URI: http://
-    Description: The Multi-Purpose Onepage Template
-    Author: UI-ThemeZ
-    Author URI: http://themeforest.net/user/UI-ThemeZ
-    Version: 1.0
+window.addEventListener('DOMContentLoaded', event => {
 
------------------------------------------------------------------------------------*/
-
-
-$(function() {
-
-    "use strict";
-
-    var wind = $(window);
-
-
-
-    // scrollIt
-    $.scrollIt({
-      upKey: 38,                // key code to navigate to the next section
-      downKey: 40,              // key code to navigate to the previous section
-      easing: 'swing',          // the easing function for animation
-      scrollTime: 600,          // how long (in ms) the animation takes
-      activeClass: 'active',    // class given to the active nav element
-      onPageChange: null,       // function(pageIndex) that is called when page is changed
-      topOffset: -80            // offste (in px) for fixed top navigation
-    });
-
-
-
-    // navbar scrolling background
-    wind.on("scroll",function () {
-
-        var bodyScroll = wind.scrollTop(),
-            navbar = $(".navbar")
-
-        if(bodyScroll > 100){
-
-            navbar.addClass("nav-scroll");
-
-        }else{
-
-            navbar.removeClass("nav-scroll");
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
         }
-    });
-
-    // navbar scrolling background
-    wind.on("scroll",function () {
-
-        var bodyScroll = wind.scrollTop(),
-            navLight = $(".nav-light"),
-            logo = $(".nav-light .logo> img");
-
-        if(bodyScroll > 100){
-
-            navLight.addClass("nav-scroll");
-            logo.attr('src', 'img/logo-dark.png');
-
-        }else{
-
-            navLight.removeClass("nav-scroll");
-            logo.attr('src', 'img/logo-light.png');
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
         }
-    });
 
+    };
 
-    // close navbar-collapse when a  clicked
-    $(".navbar-nav a").on('click', function () {
-        $(".navbar-collapse").removeClass("show");
-    });
+    // Shrink the navbar 
+    navbarShrink();
 
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
 
-    // progress bar
-    wind.on('scroll', function () {
-        $(".skill-progress .progres").each(function () {
-            var bottom_of_object = 
-            $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = 
-            $(window).scrollTop() + $(window).height();
-            var myVal = $(this).attr('data-value');
-            if(bottom_of_window > bottom_of_object) {
-                $(this).css({
-                  width : myVal
-                });
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
             }
         });
     });
 
-
-    // sections background image from data background
-    var pageSection = $(".bg-img, section");
-    pageSection.each(function(indx){
-        
-        if ($(this).attr("data-background")){
-            $(this).css("background-image", "url(" + $(this).data("background") + ")");
-        }
-    });
-
-
-    // magnificPopup
-    $('.gallery').magnificPopup({
-        delegate: '.popimg',
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
-
-
-});
-
-
-// === window When Loading === //
-
-$(window).on("load",function (){
-
-    var wind = $(window);
-
-    // Preloader
-    $(".loading").fadeOut(500);
-
-
-    // stellar
-    wind.stellar();
-
-
-    // isotope
-    $('.gallery').isotope({
-      // options
-      itemSelector: '.items',
-      percentPosition: true,
-      masonry: {
-        // use element for option
-        columnWidth: '.width2'
-      }
-    });
-
-    var $gallery = $('.gallery').isotope({
-      // options
-    });
-
-    // filter items on button click
-    $('.filtering').on( 'click', 'span', function() {
-
-        var filterValue = $(this).attr('data-filter');
-
-        $gallery.isotope({ filter: filterValue });
-
-    });
-
-    $('.filtering').on( 'click', 'span', function() {
-
-        $(this).addClass('active').siblings().removeClass('active');
-
-    });
-
-
-    // contact form validator
-    $('#contact-form').validator();
-
-    $('#contact-form').on('submit', function (e) {
-        if (!e.isDefaultPrevented()) {
-            var url = "contact.php";
-
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    if (messageAlert && messageText) {
-                        $('#contact-form').find('.messages').html(alertBox);
-                        $('#contact-form')[0].reset();
-                    }
-                }
-            });
-            return false;
-        }
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.lightBoxVideoLink'
     });
 
 });
