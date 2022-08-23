dcf325880dd2de4f9d015161b8a8ad4a48e74734
Btaft6278 <btaft6278@gmail.com>
2022-08-22 20:00:21 -0400

    playing with particleJS

---
+++ HTML/js/particles.js
@@ -0,0 +1,80 @@
+const falling = {
+    "particles": {
+        "number": {
+            "value": 30,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": ["circle", "circle", "image"],
+            "image": {
+                "src": "./assets/img/star.png",
+                "height": 1000,
+                "width": 100
+            },
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+        },
+        "opacity": {
+            "value": .5,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 2,
+                "opacity_min": 0,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": .5,
+                "size_min": 0.3,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": false
+        },
+        "move": {
+            "enable": true,
+            "speed": .5,
+            "direction": "bottom",
+            "random": true,
+            "straight": true,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 600
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": false,
+                "mode": "bubble"
+            },
+            "onclick": {
+                "enable": false,
+                "mode": "repulse"
+            },
+            "resize": true
+        }
+    },
+    "retina_detect": true
+}
+
+particlesJS("falling", falling)
