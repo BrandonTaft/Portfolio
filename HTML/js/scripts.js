fdf6180980594ff33d96a8be5a648295e9f31832
Brandon Taft <btaftcan@gmail.com>
2025-01-05 18:10:06 -0500

    2025 refresh

--- HTML/js/scripts.js
+++ HTML/js/scripts.js
@@ -1,11 +1,3 @@
-/*!
-* Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
